848f02398259e14aae85e6e9db7ef127da95288b
GeorgeLIu <g5321247@gmail.com>
2020-06-01 00:43:09 +0800

    [feat] create Constant file to collect constant variable

---
+++ AccessAppExercise/Unit/Constant.swift
@@ -0,0 +1,16 @@
+//
+//  Constant.swift
+//  AccessAppExercise
+//
+//  Created by George Liu on 2020/6/1.
+//  Copyright © 2020 George Liu. All rights reserved.
+//
+
+import Foundation
+
+struct Constant {
+
+    static var APIHost: String {
+        return "api.github"
+    }
+}
