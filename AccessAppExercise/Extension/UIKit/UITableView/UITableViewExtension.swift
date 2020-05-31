ed90b88f1705214467d3cd01c3bdb9dcd4bd04f9
GeorgeLIu <g5321247@gmail.com>
2020-05-31 23:00:35 +0800

    [feat] create UITableViewExtension file

---
+++ AccessAppExercise/Extension/UIKit/UITableView/UITableViewExtension.swift
@@ -0,0 +1,30 @@
+//
+//  UITableViewExtension.swift
+//  AccessAppExercise
+//
+//  Created by George Liu on 2020/5/31.
+//  Copyright © 2020 George Liu. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    func register(forCellReuseIdentifiers identifiers: [String]) {
+        for identifier in identifiers {
+            let nibCell = UINib(nibName: identifier, bundle: nil)
+            register(nibCell, forCellReuseIdentifier: identifier)
+        }
+    }
+
+    func makeCell<T: UITableViewCell>(indexPath: IndexPath) -> T {
+        print(String(describing: T.self))
+        guard let cell = dequeueReusableCell(
+                withIdentifier: String(describing: T.self),
+                for: indexPath
+            ) as? T else {
+                fatalError("can't find \(String(describing: T.self)) file")
+        }
+
+        return cell
+    }
+}
