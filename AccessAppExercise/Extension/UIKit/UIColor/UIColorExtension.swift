e7045c46a178da713313c97c47cbe46a4387d5f4
GeorgeLIu <g5321247@gmail.com>
2020-05-31 23:43:36 +0800

    [feat] add UIColorExtension

---
+++ AccessAppExercise/Extension/UIKit/UIColor/UIColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIColorExtension.swift
+//  AccessAppExercise
+//
+//  Created by George Liu on 2020/5/31.
+//  Copyright © 2020 George Liu. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let shadow: UIColor = UIColor(
+        red: 51.0 / 255.0,
+        green: 51.0 / 255.0,
+        blue: 51.0 / 255.0,
+        alpha: 0.6
+    )
+}
