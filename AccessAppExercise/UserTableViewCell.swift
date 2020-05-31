6a31cffbf91c8ad47565f23e6a38b8c72572c357
GeorgeLIu <g5321247@gmail.com>
2020-05-31 22:08:24 +0800

    [feat] create UserTableViewCell

---
+++ AccessAppExercise/UserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  AccessAppExercise
+//
+//  Created by George Liu on 2020/5/31.
+//  Copyright © 2020 George Liu. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var personalPictureImageView: UIImageView!
+    @IBOutlet weak var personalNameLabel: UILabel!
+    @IBOutlet weak var administrationLabel: UILabel!
+    @IBOutlet weak var baseViuew: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        selectionStyle = .none
+    }
+
+}
