ecc0e881ca2d1e481d25bd712afe8138070fc7f8
GeorgeLIu <g5321247@gmail.com>
2020-05-31 22:10:04 +0800

    [feat] create tableView and display User Cell on UIViewController

--- AccessAppExercise/ViewController.swift
+++ AccessAppExercise/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: String(describing: UserTableViewCell.self), bundle: nil), forCellReuseIdentifier: String(describing: UserTableViewCell.self))
+    }
 
 }
 
+// MARK: - TableViewDelegate, TableViewDataSource
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: UserTableViewCell.self), for: indexPath) as!
+        UserTableViewCell
+        return cell
+    }
+}
