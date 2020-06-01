d20b3b01593fa474a8c1b41632e371a1d763d9c4
GeorgeLIu <g5321247@gmail.com>
2020-06-01 14:12:21 +0800

    [feat] create URLExtesion to get parameter

---
+++ AccessAppExercise/Extension/Foundation/URLExtension.swift
@@ -0,0 +1,20 @@
+//
+//  URLExtension.swift
+//  AccessAppExercise
+//
+//  Created by George Liu on 2020/6/1.
+//  Copyright © 2020 George Liu. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    public var queryParameters: [String: String]? {
+        guard
+            let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
+            let queryItems = components.queryItems else { return nil }
+        return queryItems.reduce(into: [String: String]()) { (result, item) in
+            result[item.name] = item.value
+        }
+    }
+}
