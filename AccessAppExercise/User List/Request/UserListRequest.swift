cb877fbbc6c8f06a12961d8b64c8bdf18a6acf48
GeorgeLIu <g5321247@gmail.com>
2020-06-01 10:35:58 +0800

    [feat] create UserListRequest to handle get AllUsers request

---
+++ AccessAppExercise/User List/Request/UserListRequest.swift
@@ -0,0 +1,27 @@
+//
+//  UserListRequest.swift
+//  AccessAppExercise
+//
+//  Created by George Liu on 2020/6/1.
+//  Copyright © 2020 George Liu. All rights reserved.
+//
+
+import Foundation
+
+struct UserListRequest: Request {
+    typealias Response = [UserModel]
+    var method: HTTPMethod = .get
+    var endPoint: EndPoint = .users
+    var parameter: [String : Any] {
+        return [
+            "since": parameters.since,
+            "per_page": parameters.perPage
+        ]
+    }
+    var parameters: Parameter
+
+    struct Parameter {
+        let since: Int
+        let perPage: String
+    }
+}
